3a4813594874a871e19aff96e8cc04cb09e75eb0
SimonTE21D <112383428+SimonTE21D@users.noreply.github.com>
2023-03-06 10:37:29 +0100

    Form lable
    asdasdasd

---
+++ Lable event/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Lable_event
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(123, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 133);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Klicka här för att ändra min bakgrund färg";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label3_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(242, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 133);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Klicka här för att ändra formens bakgrund färg";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label4_MouseClick);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Orange;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(500, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(225, 133);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Klicka här";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            this.label6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label6_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.LightBlue;
+            this.label7.Location = new System.Drawing.Point(12, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(207, 133);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Kom Hit!!";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.label7.MouseEnter += new System.EventHandler(this.label7_MouseEnter);
+            this.label7.MouseLeave += new System.EventHandler(this.label7_MouseLeave);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.LightBlue;
+            this.label9.Location = new System.Drawing.Point(274, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(207, 133);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Kom Hit!!";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.MouseEnter += new System.EventHandler(this.label9_MouseEnter);
+            this.label9.MouseLeave += new System.EventHandler(this.label9_MouseLeave);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Yellow;
+            this.label8.Location = new System.Drawing.Point(520, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(216, 133);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Klicka här för 1 000 000kr";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.MouseEnter += new System.EventHandler(this.label8_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(813, 472);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Lable event/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lable_event
+{
+    public partial class Form1 : Form
+    {
+
+        int x = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.BackColor = Color.Red;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_MouseClick(object sender, MouseEventArgs e)
+        {
+            label3.BackColor = Color.Magenta;
+            label3.ForeColor = Color.White;
+        }
+
+        private void label4_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.BackColor = Color.Magenta;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            x++;
+            this.Text = x.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_MouseEnter(object sender, EventArgs e)
+        {
+            label9.Text = "Tack för att du besökte oss!";
+        }
+
+        private void label7_MouseLeave(object sender, EventArgs e)
+        {
+            label9.Text = "Kom hit!!";
+        }
+
+        private void label9_MouseEnter(object sender, EventArgs e)
+        {
+            label7.Text = "Tack för att du besökte oss!";
+        }
+
+        private void label9_MouseLeave(object sender, EventArgs e)
+        {
+            label7.Text = "Kom hit!!";
+        }
+
+        private void label8_MouseHower(object sender, EventArgs e)
+        {
+
+            int x, y;
+            Random random = new Random();
+                x = random.Next(0, 800);
+                y = random.Next(0, 500);
+
+                label8.Top = y;
+                label8.Left = x;
+
+                
+        }
+    }
+}
